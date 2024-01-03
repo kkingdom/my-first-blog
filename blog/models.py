91e6131e54b7d33fcd6a5f7ee22d6f11ed093333
llingdom <minkai.cao@gmail.com>
2024-01-03 10:00:04 +0800

    blog post model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils import timezone
+from django.conf import settings
+
 
 # Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    create_date = models.DateTimeField(black=True, null=True)
+    published_date = models.DateTimeField(black=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
