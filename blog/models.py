69dcc7a13e1b80b96cb1c493c8d896d9420772ea
llingdom <minkai.cao@gmail.com>
2024-01-03 10:01:27 +0800

    blog post model

--- blog/models.py
+++ blog/models.py
@@ -9,8 +9,8 @@ class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
-    create_date = models.DateTimeField(black=True, null=True)
-    published_date = models.DateTimeField(black=True, null=True)
+    create_date = models.DateTimeField(blank=True, null=True)
+    published_date = models.DateTimeField(blank=True, null=True)
 
     def publish(self):
         self.published_date = timezone.now()
