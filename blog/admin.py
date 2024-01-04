5cde2f808d2dffcfaf9d3543d0068db5b9c3fe32
llingdom <minkai.cao@gmail.com>
2024-01-04 08:45:57 +0800

    20240104 Jim

--- blog/admin.py
+++ blog/admin.py
@@ -3,9 +3,3 @@ from .models import Post
 # Register your models here.
 
 admin.site.register(Post)
-admin.site.register(Post)
-admin.site.register(Post)
-admin.site.register(Post)
-admin.site.register(Post)
-admin.site.register(Post)
-admin.site.register(Post)
